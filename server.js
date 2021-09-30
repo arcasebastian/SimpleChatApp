301d9835eb8d58c35dc0ae366ca8e015c2d15597
sebastian <sebasarca711@gmail.com>
2021-09-30 20:26:26 -0300

    load mockup Messages with axios

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.use(express.static(__dirname));
+
+const messages = [
+  {
+    name: "Tim",
+    text: "Hello",
+  },
+  {
+    name: "Maria",
+    text: "Hello",
+  },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
