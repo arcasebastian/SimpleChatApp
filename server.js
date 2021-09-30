e07bd726d79cfcdd811f3f8ecc88c13837696a8d
sebastian <sebasarca711@gmail.com>
2021-09-30 20:19:24 -0300

    Set frontend file

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 app.listen(port, () => {
-    console.log(`Server is listening on port: ${port}`)
-})
-
+  console.log(`Server is listening on port: ${port}`);
+});
