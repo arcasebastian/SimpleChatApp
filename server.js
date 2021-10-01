c1393520df6903b7c30a49c6c0ec9dd0563e84a5
sebastian <sebasarca711@gmail.com>
2021-09-30 22:58:02 -0300

    Save chat history to mongodb

--- server.js
+++ server.js
@@ -3,18 +3,30 @@ const bodyParser = require("body-parser");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const mongoose = require("mongoose");
+const credentials = require("./credentials.json");
 
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-const messages = [];
+const dbUrl = `mongodb+srv://${credentials.dbUser}:${credentials.dbPass}@${credentials.cluster}?retryWrites=true&w=majority`;
+const MessageModel = mongoose.model("Message", {
+  name: { type: String, required: true },
+  text: { type: String, required: true },
+});
 
 app.get("/messages", (req, res) => {
-  res.send(messages);
+  MessageModel.find({}, (err, messages) => {
+    if (err) res.sendStatus(500);
+    res.send(messages);
+  });
 });
-app.post("/messages", (req, res) => {
-  messages.push(req.body);
+app.post("/messages", async (req, res) => {
+  const newMessage = new MessageModel(req.body);
+  await newMessage.save().catch((err) => {
+    res.sendStatus(500);
+  });
   io.emit("message", req.body);
   res.sendStatus(200);
 });
@@ -22,7 +34,9 @@ app.post("/messages", (req, res) => {
 io.on("connection", (socket) => {
   console.log(`Connected client on: ${socket.conn.remoteAddress}`);
 });
-
+mongoose.connect(dbUrl, {}, (error) => {
+  console.log("Connection error", error);
+});
 http.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
