84b529f08257d4e62b3b2aa19afd974de6c1a963
sebastian <sebasarca711@gmail.com>
2021-09-30 21:00:17 -0300

    postMessages

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 const messages = [
   {
@@ -16,6 +18,10 @@ const messages = [
 
 app.get("/messages", (req, res) => {
   res.send(messages);
+});
+app.post("/messages", (req, res) => {
+  messages.push(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
