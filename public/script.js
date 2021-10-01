9b1a908b1c3ecc66626be4e926f7c4f9ae2bc6c5
sebastian <sebasarca711@gmail.com>
2021-09-30 21:33:14 -0300

    Refactoring file structure

---
+++ public/script.js
@@ -0,0 +1,56 @@
+const messageForm = document.querySelector("#send-message");
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = formData();
+  if (!data) return;
+  postMessage(data).then(() => {
+    addMesagges(data);
+  });
+});
+
+function addMesagges(message) {
+  const messagesArea = document.querySelector(".messages");
+  messagesArea.append(formatMesage(message));
+}
+
+function formatMesage({ name, text }) {
+  const listElement = document.createElement("li");
+  listElement.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-start"
+  );
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("ms-2", "me-auto");
+  const messageHeader = document.createElement("div");
+  messageHeader.classList.add("fw-bold");
+  messageHeader.innerHTML = `<h4>${name}</h4>`;
+  const time = document.createElement("span");
+  time.innerText = new Date().toLocaleString();
+  messageElement.append(messageHeader, text);
+  listElement.append(messageElement, time);
+  return listElement;
+}
+
+async function getMessages() {
+  const newMessages = await axios.get("/messages");
+  for (let message of newMessages.data) {
+    addMesagges(message);
+  }
+}
+async function postMessage({ name, text }) {
+  await axios.post("/messages", { name, text });
+}
+const formData = function () {
+  const nameInput = document.querySelector("#name");
+  const textMessage = document.querySelector("#text");
+  const response = {};
+  for (let input of [nameInput, textMessage]) {
+    const { name, value } = input;
+    response[name] = value;
+  }
+  return response;
+};
+getMessages();
